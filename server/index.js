82fc5b6ba219d51df01a5605ac2355b02a0b2220
Vashishth Sharma <vashishthsharma1793@gmail.com>
2026-07-31 20:49:47 +0530

    Add about route and prepare first API

--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
-// Import Express
 const express = require("express");
 
-// Create Express application
 const app = express();
 
-// Port number
 const PORT = 5000;
 
 // Home Route
@@ -12,6 +9,17 @@ app.get("/", (req, res) => {
     res.send("LifeLink Backend is Running 🚀");
 });
 
+// About Route
+app.get("/about", (req, res) => {
+    res.send("Welcome to LifeLink Backend");
+});
+app.get("/api", (req, res) => {
+    res.json({
+        success: true,
+        message: "LifeLink API is Working",
+        version: "1.0.0"
+    });
+});
 // Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
